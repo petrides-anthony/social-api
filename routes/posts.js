f06a8fc15a45204c49d476a3ba63839a265bf139
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-06-24 17:59:51 +0100

    adding try catch to remaining post functions

--- routes/posts.js
+++ routes/posts.js
@@ -7,34 +7,47 @@ const router = express.Router();
 
 // GET /
 // get all posts
+
 router.get('/', async (req, res, next) => {
-  const allPosts = await PostModel.find();
-  res.json(allPosts);
-  next();
+  try {
+    const allPosts = await PostModel.find();
+    res.json(allPosts);
+    next();
+  } catch (error) {
+    console.log('Error from GET all posts ->', error)
+    next(error)
+  }
 });
 
 // POST /
 // add a new post
 router.post('/', async (req, res, next) => {
   const { user, content } = req.body || {};
-  assert.ok(user !== undefined, new BadRequest('`user` is missing'));
-  assert.ok(typeof user === 'string', new BadRequest('`user` needs to be of type string'));
-  assert.ok(content !== undefined, new BadRequest('`content` is missing'));
-  assert.ok(typeof content === 'string', new BadRequest('`content` needs to be of type string'));
+  
+  try {
+    assert.ok(user !== undefined, new BadRequest('`user` is missing'));
+    assert.ok(typeof user === 'string', new BadRequest('`user` needs to be of type string'));
+    assert.ok(content !== undefined, new BadRequest('`content` is missing'));
+    assert.ok(typeof content === 'string', new BadRequest('`content` needs to be of type string'));
+  
+    // create the post object
+    const post = new PostModel({
+      user:  user,
+      content: content
+    });
+  
+    // saved in the db
+    const savedPost = await post.save();
+  
+    // return saved data from the db to the client
+    res.json(savedPost);
+  
+    next();
+  } catch (error) {
+    console.log('Error from POST /:id ->', error);
+    return next(error);
+  }
 
-  // create the post object
-  const post = new PostModel({
-    user:  user,
-    content: content
-  });
-
-  // saved in the db
-  const savedPost = await post.save();
-
-  // return saved data from the db to the client
-  res.json(savedPost);
-
-  next();
 });
 
 // GET /:id
