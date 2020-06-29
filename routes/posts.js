64f434625c3ab4e611b88396fa583a521bce8b7b
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-06-29 09:04:11 +0100

    begin adding post comment

--- routes/posts.js
+++ routes/posts.js
@@ -72,17 +72,39 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-/*
-TRY
-  |
-  |
-  | findById
-  |
-CATCH
-  |
-*/
-// Run the above get posts/id and retrieve the json with that id using Mongooses findById method per below
-// Return the object, or 404 from express
-// https://mongoosejs.com/docs/api.html#model_Model.findById
+// POST
+// Add a comment
+
+router.post('/:id/comment', async (req, res, next) => {
+  const { user, content, parentId } = req.body || {};
+
+  try {
+    assert.ok(user !== undefined, new BadRequest('`user` is missing'));
+    assert.ok(typeof user === 'string', new BadRequest('`user` needs to be of type string'));
+    assert.ok(content !== undefined, new BadRequest('`content` is missing'));
+    assert.ok(typeof content === 'string', new BadRequest('`content` needs to be of type string'));
+    assert.ok(parentId !== undefined, new BadRequest('`parentId` is missing'));
+    assert.ok(typeof parentId === 'string', new BadRequest('`parentId` needs to be of type string'));
+
+    // create the post object
+    const post = new PostModel({
+      user: user,
+      content: content,
+      parentId: req.params.id
+    });
+
+    // saved in the db
+    const savedPost = await post.save();
+  
+    // return saved data from the db to the client
+    res.json(savedPost);
+
+    next();
+  } catch (error) {
+    console.log('Error from POST/:id/comment ->', error);
+    return next(error);
+  }
+
+});
 
 module.exports = router;
