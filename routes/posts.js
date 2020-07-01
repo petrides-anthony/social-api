7a3594221867ddb0384705d49d29c69e848d657c
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-07-01 17:44:05 +0100

    add tasks to do before next session

--- routes/posts.js
+++ routes/posts.js
@@ -127,6 +127,9 @@ module.exports = router;
 // *** To Do's ***
 // - Update Get Posts to only get posts where parentId is null [ x ]
 // - Add a Get Comments function [ ]
+// - Add a Get All Comments for a given post function [ ]
+// - Add a Like function /posts/id/like [ ] Note: Mongo comes with an increment feature for this...
+// ...so you don't have to do like = likes +1
 // - Update Add Comments function to only add comment if req.param.id exists for a post [x]
 // - Update Add Comments function so that it updates the parent post's 'lastestComment' attribute [x]
 
